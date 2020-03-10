64c0d8244ef95d041c937a59b734454d71565d3a
Garpur3 <garpurhnefill@gmail.com>
2020-03-10 17:14:50 +0000

    Changed value to values in zip iterable and fixed f-string functionality

--- test_sample.py
+++ test_sample.py
@@ -4,9 +4,9 @@ numbers = [23, 20, 21, -21, 10, 15, 45, -45, -20, 2, 100]
 
 values = [23, "Buzz", "Fizz", -21, "Buzz", "FizzBuzz", "FizzBuzz", -45, -20, 2, "Buzz"]
 
-for number, value in zip(numbers, value):
+for number, value in zip(numbers, values):
     foo_val = FizzBuzz(number)
     if foo_val == value:
-        print("Function works for {number} and returns {foo_val}")
+        print(f"Function works for {number} and returns {foo_val}")
     else:
-        print("Unexpected return value for {number}: {foo_val}")
+        print(f"Unexpected return value for {number}: {foo_val}")
